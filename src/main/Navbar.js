95073539a729e8e2928be6177826ab643580ca55
klb545 <126502552+klb545@users.noreply.github.com>
2023-06-08 08:39:35 +0100

    adds note

--- src/main/Navbar.js
+++ src/main/Navbar.js
@@ -1,7 +1,10 @@
 const Navbar = () => {
     
     return ( 
-        <div className="navbar"><h1>example</h1></div>
+        <div className="navbar">
+            <h1>example</h1>
+            <p>console.log("[....]") when thief bumps into painting</p>
+        </div>
      );
 }
  
