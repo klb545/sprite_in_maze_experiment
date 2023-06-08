ad3a68292ff5d24dd12e77c8d63c7f7a688c55c9
klb545 <126502552+klb545@users.noreply.github.com>
2023-06-08 08:28:50 +0100

    shows component diagram

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './main/Navbar';
+import componentDiagram from './images/componentDiagram.png'
 import MainContainer from './main/MainContainer';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       {/* <Navbar/> */}
       <MainContainer/>
+      <img src={componentDiagram}/>
     </div>
   );
 }
